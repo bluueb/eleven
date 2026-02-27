e5d02b4305405872ab91ecff732300d5c99bf55b
client0sided <safiilkemert11@gmail.com>
2026-02-27 08:28:06 +0000

    clean urls

--- server.js
+++ server.js
@@ -223,3 +223,6 @@ io.on("connection", (socket) => {
 })
 
 server.listen(3000, () => { console.log("Server running on port 3000") })
+app.get("/game", (req, res) => {
+  res.sendFile(__dirname + "/public/page.html")
+})
